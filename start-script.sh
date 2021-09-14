9167c7bdfafdc64a7a03da80666248211741e020
mshukla-intel <mangalx.shukla@intel.com>
2021-09-14 12:40:27 +0530

    Update start-script.sh

--- start-script.sh
+++ start-script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-oc login --token=$TOKEN --server=$URL --insecure-skip-tls-verify=true
+oc login --token=sha256~mUouiAYWFrRHpS6uLGnXRxhX81FbcEpgTSxpw39MZlY --server=https://api.cfa.devcloud.intel.com:6443 --insecure-skip-tls-verify=true
 oc debug node/$(oc get pods -o json |jq '.items[].spec.nodeName'|sed 's/"//g') -T -- chroot
 /host sh -c "/etc/nodecleanup.sh"
