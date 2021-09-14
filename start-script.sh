e53f1355a62024dc453121136a2dd63fef34a322
mshukla-intel <mangalx.shukla@intel.com>
2021-09-14 12:50:18 +0530

    addded output

--- start-script.sh
+++ start-script.sh
@@ -2,3 +2,4 @@
 oc login --token=sha256~mUouiAYWFrRHpS6uLGnXRxhX81FbcEpgTSxpw39MZlY --server=https://api.cfa.devcloud.intel.com:6443 --insecure-skip-tls-verify=true
 oc debug node/$(oc get pods -o json |jq '.items[].spec.nodeName'|sed 's/"//g') -T -- chroot
 /host sh -c "/etc/nodecleanup.sh"
+echo "============================  node cleanup successful =================== "
