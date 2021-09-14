5cda40b48905e3a03ef73d0589b89c6202008996
mshukla-intel <mangalx.shukla@intel.com>
2021-09-14 16:49:44 +0530

    added some msg to see on console

--- start-script.sh
+++ start-script.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+echo "============================  crictl images output before clean up =================== "
+crictl images
+echo "============================  crictl images output before clean up =================== "
 oc login --token=sha256~mUouiAYWFrRHpS6uLGnXRxhX81FbcEpgTSxpw39MZlY --server=https://api.cfa.devcloud.intel.com:6443 --insecure-skip-tls-verify=true
 oc debug node/$(oc get pods -o json |jq '.items[].spec.nodeName'|sed 's/"//g') -T -- chroot
+echo "============================  node cleanup invoked =================== "
 /host sh -c "/etc/nodecleanup.sh"
+echo "============================  crictl images output AFTER =================== "
+crictl images
+echo "============================  crictl images output AFTER =================== "
 echo "============================  node cleanup successful =================== "
