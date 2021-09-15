f03e487620d085a8b4c5b04ed33cc49b9d4b977b
mshukla-intel <mangalx.shukla@intel.com>
2021-09-15 11:25:07 +0530

    Update start-script.sh

--- start-script.sh
+++ start-script.sh
@@ -3,9 +3,8 @@ echo "============================  crictl images output before clean up =======
 crictl images
 echo "============================  crictl images output before clean up =================== "
 oc login --token=sha256~4GLYZShVt4rDYgRVsJr3KMmxfSG3OvlAvxMYpjn1rEE --server=https://api.cfa.devcloud.intel.com:6443 --insecure-skip-tls-verify=true
-oc debug node/$(oc get pods -o json |jq '.items[].spec.nodeName'|sed 's/"//g') -T -- chroot
 echo "============================  node cleanup invoked =================== "
-/host sh -c "/etc/nodecleanup.sh"
+oc debug node/$(oc get pods -o json |jq '.items[].spec.nodeName'|sed 's/"//g') -T -- chroot /host sh -c "/etc/nodecleanup.sh"
 echo "============================  crictl images output AFTER =================== "
 crictl images
 echo "============================  crictl images output AFTER =================== "
