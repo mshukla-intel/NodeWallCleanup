59248f9384215e917074e57703f07bf319408564
mshukla-intel <mangalx.shukla@intel.com>
2021-09-16 21:17:16 +0530

    env for node_name and token

--- start-script.sh
+++ start-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "============================  oc login =================== "
-oc login --token=sha256~a-nBb0cwJolBlpO6H42hXxBv8rrkNC-OU1S5WOI2S84 --server=https://api.cfa.devcloud.intel.com:6443 --insecure-skip-tls-verify=true
+oc login --token=$TOKEN --server=https://api.cfa.devcloud.intel.com:6443 --insecure-skip-tls-verify=true
 echo "============================  node cleanup invoking =================== "
-oc debug node/worker26 -T -- chroot /host sh -c "/etc/nodecleanup.sh"
+oc debug node/$NODE_NAME -T -- chroot /host sh -c "/etc/nodecleanup.sh"
 echo "============================  node cleanup successful =================== "
