2350c111233741eb6cd3fc48760c3e9afc202c1d
mshukla-intel <mangalx.shukla@intel.com>
2021-09-17 12:29:44 +0530

    inserted node cleanup script

--- start-script.sh
+++ start-script.sh
@@ -2,5 +2,7 @@
 echo "============================  oc login =================== "
 oc login --token=$TOKEN --server=https://api.cfa.devcloud.intel.com:6443 --insecure-skip-tls-verify=true
 echo "============================  node cleanup invoking =================== "
-oc debug node/$NODE_NAME -T -- chroot /host sh -c "/etc/nodecleanup.sh"
+oc debug node/$NODE_NAME -T -- chroot /host sh -c 
+oc debug crictl rmi --prune
+oc debug for id in `crictl ps -a | grep -i exited | awk '{print $1}'`; do crictl rm $id ; done
 echo "============================  node cleanup successful =================== "
