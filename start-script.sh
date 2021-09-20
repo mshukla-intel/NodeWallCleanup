cd6850ab72fc1114c1aa5fe4e805900106acab11
mshukla-intel <mangalx.shukla@intel.com>
2021-09-20 17:04:33 +0530

    echo loop data

--- start-script.sh
+++ start-script.sh
@@ -5,7 +5,10 @@ echo "============================  node cleanup invoking =================== "
 OUTPUT=$(oc debug node/worker26 -T -- chroot /host bash -c "crictl ps -a | grep -i exited" | awk '{print $1}')
 echo " About to loop through OUTPUT... "
 echo $OUTPUT
-for i in "${OUTPUT[@]}" do echo $i done
+for id in "${OUTPUT[@]}"
+do
+    echo $id
+done
 #oc debug for id in OUTPUT; do crictl rm $id ; done
 #oc debug node/$NODE_NAME -T -- chroot /host sh -c "for id in `crictl ps -a | grep -i exited | awk '{print $1}'`; do crictl rm $id ; done"
 echo "============================  node cleanup successful =================== "
