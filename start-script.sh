dbcf097d8b4aa8c80537a8d190a81977aa217547
mshukla-intel <mangalx.shukla@intel.com>
2021-09-20 12:09:41 +0530

    added -T -- chroot /host

--- start-script.sh
+++ start-script.sh
@@ -2,7 +2,7 @@
 echo "============================  oc login =================== "
 oc login --token=$TOKEN --server=https://api.cfa.devcloud.intel.com:6443 --insecure-skip-tls-verify=true
 echo "============================  node cleanup invoking =================== "
-oc debug node/$NODE_NAME -T 
+oc debug node/$NODE_NAME -T -- chroot /host
 oc debug crictl rmi --prune
 oc debug for id in `crictl ps -a | grep -i exited | awk '{print $1}'`; do crictl rm $id ; done
 echo "============================  node cleanup successful =================== "
