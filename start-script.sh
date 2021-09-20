1ed00b13d8d9439958f42e1edc29eab3f34b44e9
mshukla-intel <mangalx.shukla@intel.com>
2021-09-20 13:07:14 +0530

    Update start-script.sh

--- start-script.sh
+++ start-script.sh
@@ -3,5 +3,5 @@ echo "============================  oc login =================== "
 oc login --token=$TOKEN --server=https://api.cfa.devcloud.intel.com:6443 --insecure-skip-tls-verify=true
 echo "============================  node cleanup invoking =================== "
 oc debug node/$NODE_NAME -T -- chroot /host sh -c "crictl rmi --prune"
-# oc debug for id in `crictl ps -a | grep -i exited | awk '{print $1}'`; do crictl rm $id ; done
+oc debug node/$NODE_NAME -T -- chroot /host sh -c "for id in `crictl ps -a | grep -i exited | awk '{print $1}'`; do crictl rm $id ; done"
 echo "============================  node cleanup successful =================== "
