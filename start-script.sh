204f4a17f245e24ca4d6c97391731f05f1f65f46
mshukla-intel <mangalx.shukla@intel.com>
2021-09-20 19:25:54 +0530

    updated loop logic to iterate or elemnts

--- start-script.sh
+++ start-script.sh
@@ -2,15 +2,19 @@
 echo "============================  oc login =================== "
 oc login --token=$TOKEN --server=https://api.cfa.devcloud.intel.com:6443 --insecure-skip-tls-verify=true
 echo "============================  node cleanup invoking rmi prune =================== "
-oc debug node/$NODE_NAME -T -- chroot /host sh -c "crictl rmi --prune"
+#oc debug node/$NODE_NAME -T -- chroot /host sh -c "crictl rmi --prune"
 echo "============================  obtaining list of exited resources =================== "
-OUTPUT=$(oc debug node/worker26 -T -- chroot /host bash -c "crictl ps -a | grep -i exited" | awk '{print $1}')
+oc debug node/worker26 -T -- chroot /host bash -c "crictl ps -a | grep -i exited" | awk '{print $1}' > out.txt
 echo "============================  loop through OUTPUT... =================== "
-echo $OUTPUT
-for id in "${OUTPUT[@]}"
-do
-    oc debug node/$NODE_NAME -T -- chroot /host sh -c "crictl rm $id"
+for id in $(cat out.txt);
+    do
+    echo "Ganesha..."
+    echo "$i clean up";
+#    oc debug node/$NODE_NAME -T -- chroot /host sh -c "crictl rm $id"
 done
+rm -rf out.txt
+
+# ARCHIVED commands
 #oc debug for id in OUTPUT; do crictl rm $id ; done
 #oc debug node/$NODE_NAME -T -- chroot /host sh -c "for id in `crictl ps -a | grep -i exited | awk '{print $1}'`; do crictl rm $id ; done"
 echo "============================  node cleanup successful =================== "
