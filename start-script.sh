01926417f3e708f89b5bb57e3e8a8ad5ec51f8f9
mshukla-intel <mangalx.shukla@intel.com>
2021-09-22 12:48:36 +0530

    /tmp/out.txt

--- start-script.sh
+++ start-script.sh
@@ -4,13 +4,13 @@ oc login --token=$TOKEN --server=https://api.cfa.devcloud.intel.com:6443 --insec
 echo "============================  node cleanup invoking rmi prune =================== "
 oc debug node/$NODE_NAME -T -- chroot /host sh -c "crictl rmi --prune"
 echo "============================  obtaining list of exited resources =================== "
-oc debug node/worker26 -T -- chroot /host bash -c "crictl ps -a | grep -i exited" | awk '{print $1}' > out.txt
+oc debug node/worker26 -T -- chroot /host bash -c "crictl ps -a | grep -i exited" | awk '{print $1}' > /tmp/out.txt
 echo "============================  loop through OUTPUT... =================== "
-for id in $(cat out.txt);
+for id in $(cat /tmp/out.txt);
     do
     oc debug node/$NODE_NAME -T -- chroot /host sh -c "crictl rm $id"
 done
-rm -rf out.txt
+rm -rf /tmp/out.txt
 
 # ARCHIVED commands
 #oc debug for id in OUTPUT; do crictl rm $id ; done
